511a5548fbab6def3c226171a4f20093990f513b
idotter <marc@idotter.ch>
2026-09-02 20:14:53 +0200

    Update package.json/next.config for standalone Docker deploy; add Dockerfile and .dockerignore

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,7 @@ const withSerwist = withSerwistInit({
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "standalone",
 };
 
 export default withSerwist(withNextIntl(nextConfig));
